6ee85f89fe354dc261e0718106c607833e3fe83e
ethicnology <25751709+ethicnology@users.noreply.github.com>
2020-06-19 15:16:52 +0200

    Update FilmRepository.java

--- src/main/java/com/julescoco/allocine/FilmRepository.java
+++ src/main/java/com/julescoco/allocine/FilmRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface FilmRepository extends CrudRepository<Film, Integer> {
-    // Film findByTitre(String title);
     Film findByFilmid(Integer id);
 }
